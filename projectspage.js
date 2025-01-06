327847217124b280c7f3209616fb25adbe229993
emmanuelsam34 <emmanuelagbedejobi@gmail.com>
2025-01-06 10:57:14 +0100

    my first commit

---
+++ projectspage.js
@@ -0,0 +1,23 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: [
+                'Current Initiatives',
+                'Active Projects',
+                'Completed Projects',
+                'Archived Projects',
+                ],
+        datasets: [{
+        label: 'My First Dataset', 
+        data: [300, 50, 100, 40],
+        backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)',
+            'rgb(242, 244, 243)'
+        ],
+        hoverOffset: 4
+        }]
+    },
+});
